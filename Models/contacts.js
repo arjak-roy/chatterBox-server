d277d2b7d001a166f3a8736f15e6cdb7cdd487dd
unknown <roykalyani44@gmail.com>
2024-05-19 22:24:02 +0530

    first commit

---
+++ Models/contacts.js
@@ -0,0 +1,48 @@
+const e = require("express");
+var mongoose = require("mongoose");
+// const uri = "mongodb+srv://Arjak_real:So5EGCgjvQanIqgn@cluster0.crakkng.mongodb.net/ChatterBox";
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log(`Connected to MongoDB+${process.env.MONGODB_URI}`)); // Improved connection handling
+
+var contact = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+
+
+})
+
+const usersSchema =  mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contacts: {
+    type: [contact], // Array of Contact objects
+    required: true, // Ensure contacts are always present
+    default : []
+  },
+});
+
+module.exports = mongoose.model("User", usersSchema);
